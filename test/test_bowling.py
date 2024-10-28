c954a1536573d072b221b02750f9c815c488ed04
RoopeK <35376295+Toopemus@users.noreply.github.com>
2024-10-28 17:08:56 +0200

    user story 1 - test 4

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -16,6 +16,7 @@ class TestBowlingGame(unittest.TestCase):
     def test_get_frame_raises_on_empty_game(self):
         game = BowlingGame()
         self.assertRaises(BowlingError, game.get_frame_at, 0)
+
     def test_add_frame_can_add_10_games(self):
         game = BowlingGame()
         game.add_frame(Frame(1, 5))
@@ -30,3 +31,17 @@ class TestBowlingGame(unittest.TestCase):
         last_frame = Frame(2, 6)
         game.add_frame(last_frame)
         self.assertEqual(last_frame, game.get_frame_at(9))
+
+    def test_add_frame_raises_on_11_games(self):
+        game = BowlingGame()
+        game.add_frame(Frame(1, 5))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(7, 2))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(4, 4))
+        game.add_frame(Frame(5, 3))
+        game.add_frame(Frame(3, 3))
+        game.add_frame(Frame(4, 5))
+        game.add_frame(Frame(8, 1))
+        game.add_frame(Frame(2, 6))
+        self.assertRaises(BowlingError, game.add_frame, Frame(2,6))
